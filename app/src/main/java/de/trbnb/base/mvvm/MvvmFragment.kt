20a96fa347f5d6fd3a71fc4dd1b59feb3e98fc18
Thorben <thorben29@gmail.com>
2017-02-11 16:40:24 +0100

    Implemented changes for the MvvmActivity in the MvvmFragment as well.

--- app/src/main/java/de/trbnb/base/mvvm/MvvmFragment.kt
+++ app/src/main/java/de/trbnb/base/mvvm/MvvmFragment.kt
@@ -1,48 +1,89 @@
 package de.trbnb.base.mvvm
 
+import android.databinding.Observable
+import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.LoaderManager
 import android.support.v4.content.Loader
-import de.trbnb.apptemplate.app.App
-import de.trbnb.apptemplate.app.AppComponent
-import javax.inject.Inject
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import de.trbnb.apptemplate.BR
 import javax.inject.Provider
 
 abstract class MvvmFragment<VM : ViewModel> : Fragment(), LoaderManager.LoaderCallbacks<VM> {
 
-    protected lateinit var viewModel: VM
-
     protected abstract val loaderID: Int
 
-    @Inject
-    lateinit var viewModelLoaderProvider: Provider<ViewModelLoader<VM>>
+    protected lateinit var binding: ViewDataBinding
+    protected var viewModel: VM? = null
+        set(value) {
+            if(field === value) return
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+            field?.removeOnPropertyChangedCallback(viewModelObserver)
 
-        injectDependencies(App.appComponent)
-        initLoader()
+            field = value
+            val bindingWasSuccessful = binding.setVariable(viewModelBindingId, value)
+
+            if(!bindingWasSuccessful){
+                throw RuntimeException("Unable to set the ViewModel for the variable $viewModelBindingId.")
+            }
+
+            value?.addOnPropertyChangedCallback(viewModelObserver)
+        }
+
+    private val viewModelBindingId: Int
+        get() = BR.vm
+
+    abstract val viewModelProvider: Provider<VM>
+
+    private val viewModelObserver = object : Observable.OnPropertyChangedCallback(){
+        @Suppress("UNCHECKED_CAST")
+        override fun onPropertyChanged(sender: Observable, fieldId: Int) {
+            onViewModelPropertyChanged(sender as VM, fieldId)
+        }
     }
 
-    abstract fun injectDependencies(graph: AppComponent)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = initBinding(inflater, container)
 
-    abstract fun onViewModelLoaded(viewModel: VM)
+        initLoader()
 
+        return binding.root
+    }
+
+    protected abstract fun initBinding(inflater: LayoutInflater, container: ViewGroup?): ViewDataBinding
+
+    //region Loader
     private fun initLoader(){
         activity.supportLoaderManager.initLoader(loaderID, null, this)
     }
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<VM> {
-        return viewModelLoaderProvider.get()
+        return ViewModelLoader(context, viewModelProvider)
     }
 
     override fun onLoadFinished(loader: Loader<VM>, data: VM) {
         viewModel = data
-        onViewModelLoaded(data)
     }
 
     override fun onLoaderReset(loader: Loader<VM>?) {
-        viewModel.onDestroy()
+        // nothing to do here
+    }
+    //endregion
+
+    protected open fun onViewModelPropertyChanged(viewModel: VM, fieldId: Int){
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        viewModel?.removeOnPropertyChangedCallback(viewModelObserver)
+
+        if(activity.isFinishing){
+            viewModel?.onViewFinishing()
+        }
     }
 }
