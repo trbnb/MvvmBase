21a3dff57f63e1cfcb07a18891798bbb72428cad
Thorben Buchta <thorbenbuchta@gmail.com>
2017-03-22 23:19:37 +0100

    Updated DatabindingCommands

--- mvvmbase/src/main/java/de/trbnb/mvvmbase/CommandHelpers.kt
+++ mvvmbase/src/main/java/de/trbnb/mvvmbase/CommandHelpers.kt
@@ -1,14 +1,15 @@
 package de.trbnb.mvvmbase
 
+import android.content.Context
 import de.trbnb.databindingcommands.command.RuleCommand
 import de.trbnb.databindingcommands.command.SimpleCommand
 
 /**
  * Helper method to create a [SimpleCommand] with named parameters.
  */
-fun simpleCommand(enabled: Boolean = true, action: () -> Unit) = SimpleCommand(action, enabled)
+fun simpleCommand(enabled: Boolean = true, action: (Context) -> Unit) = SimpleCommand(action, enabled)
 
 /**
  * Helper method to create a [RuleCommand] with named parameters.
  */
-fun ruleCommand(action: () -> Unit, enabledRule: () -> Boolean) = RuleCommand(action, enabledRule)
+fun ruleCommand(action: (Context) -> Unit, enabledRule: () -> Boolean) = RuleCommand(action, enabledRule)
