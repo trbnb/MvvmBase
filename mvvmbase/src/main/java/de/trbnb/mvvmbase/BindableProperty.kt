4eeb3c15ab354f2910a4725a8c73e4a8c6d97f88
Thorben Buchta <thorbenbuchta@gmail.com>
2018-01-04 14:49:11 +0100

    fix(bindable property): make BindableProperty more generic to allow references to the holding view model

--- mvvmbase/src/main/java/de/trbnb/mvvmbase/BindableProperty.kt
+++ mvvmbase/src/main/java/de/trbnb/mvvmbase/BindableProperty.kt
@@ -12,10 +12,10 @@ import kotlin.reflect.KProperty
  * @param fieldId Value of field ID inside BR.java.
  * @param defaultValue Value that will be used at start.
  */
-class BindableProperty<T>(
+class BindableProperty<R : BaseObservable, T>(
         private val fieldId: Int,
         defaultValue: T
-) : ReadWriteProperty<BaseObservable, T> {
+) : ReadWriteProperty<R, T> {
 
     /**
      * Gets or sets the stored value.
@@ -35,7 +35,7 @@ class BindableProperty<T>(
      * This function will not be invoked if [BindableProperty.distinct] is true and the new value
      * is equal to the old value.
      */
-    internal var beforeSet: ((T, T) -> Unit)? = null
+    internal var beforeSet: (R.(T, T) -> Unit)? = null
 
     /**
      * Gets or sets a function that will validate a newly set value.
@@ -44,30 +44,30 @@ class BindableProperty<T>(
      *
      * If this function is null validation will not happen and the new value will simply be set.
      */
-    internal var validate: ((T, T) -> T)? = null
+    internal var validate: (R.(T, T) -> T)? = null
 
     /**
      * Gets or sets a function that will be invoked if a new value was set and
      * [BaseObservable.notifyPropertyChanged] was invoked.
      * The first parameter is the old value and the second parameter is the new value.
      */
-    internal var afterSet: ((T) -> Unit)? = null
+    internal var afterSet: (R.(T) -> Unit)? = null
 
-    override fun getValue(thisRef: BaseObservable, property: KProperty<*>) = value
+    override fun getValue(thisRef: R, property: KProperty<*>) = value
 
-    override fun setValue(thisRef: BaseObservable, property: KProperty<*>, value: T) {
+    override fun setValue(thisRef: R, property: KProperty<*>, value: T) {
         if (distinct && this.value == value) {
             return
         }
 
-        beforeSet?.invoke(this.value, value)
-        this.value = validate?.invoke(this.value, value) ?: value
+        beforeSet?.invoke(thisRef, this.value, value)
+        this.value = validate?.invoke(thisRef, this.value, value) ?: value
         thisRef.notifyPropertyChanged(fieldId)
-        afterSet?.invoke(value)
+        afterSet?.invoke(thisRef, value)
     }
 }
 
-fun <T> bindable(fieldId: Int, defaultValue: T): BindableProperty<T> {
+fun <R : BaseObservable, T> R.bindable(fieldId: Int, defaultValue: T): BindableProperty<R, T> {
     return BindableProperty(fieldId, defaultValue)
 }
 
@@ -75,22 +75,22 @@ fun <T> bindable(fieldId: Int, defaultValue: T): BindableProperty<T> {
  * Sets [BindableProperty.distinct] of a [BindableProperty] instance to `true` and returns that
  * instance.
  */
-fun <T> BindableProperty<T>.distinct() = apply { distinct = true }
+fun <R : BaseObservable, T> BindableProperty<R, T>.distinct() = apply { distinct = true }
 
 /**
  * Sets [BindableProperty.beforeSet] of a [BindableProperty] instance to a given function and
  * returns that instance.
  */
-fun <T> BindableProperty<T>.beforeSet(action: (T, T) -> Unit) = apply { beforeSet = action }
+fun <R : BaseObservable, T> BindableProperty<R, T>.beforeSet(action: R.(T, T) -> Unit) = apply { beforeSet = action }
 
 /**
  * Sets [BindableProperty.validate] of a [BindableProperty] instance to a given function and
  * returns that instance.
  */
-fun <T> BindableProperty<T>.validate(action: (T, T) -> T) = apply { validate = action }
+fun <R : BaseObservable, T> BindableProperty<R, T>.validate(action: R.(T, T) -> T) = apply { validate = action }
 
 /**
  * Sets [BindableProperty.afterSet] of a [BindableProperty] instance to a given function and
  * returns that instance.
  */
-fun <T> BindableProperty<T>.afterSet(action: (T) -> Unit) = apply { afterSet = action }
+fun <R : BaseObservable, T> BindableProperty<R, T>.afterSet(action: R.(T) -> Unit) = apply { afterSet = action }
