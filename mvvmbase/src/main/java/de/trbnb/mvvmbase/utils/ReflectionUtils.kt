1d2f1be766423a2914413e7a6f06687ad9de56a3
Thorben Buchta <thorbenbuchta@gmail.com>
2018-11-17 08:10:28 +0100

    fix(utils): fix issues in reflection utility functions

--- mvvmbase/src/main/java/de/trbnb/mvvmbase/utils/ReflectionUtils.kt
+++ mvvmbase/src/main/java/de/trbnb/mvvmbase/utils/ReflectionUtils.kt
@@ -10,11 +10,11 @@ internal inline fun <reified T> Any.findGenericSuperclass(): ParameterizedType? 
         return genericSuperClass
     }
 
-    return genericSuperClass.findGenericSuperclass(T::class.java)
+    return genericSuperClass?.findGenericSuperclass(T::class.java)
 }
 
 private tailrec fun <T> Type.findGenericSuperclass(targetType: Class<T>): ParameterizedType? {
-    this as? Class<*> ?: return null
+    if (this !is Class<*>) return null
     val genericSuperClass = this.genericSuperclass ?: return null
 
     if (genericSuperClass is ParameterizedType && genericSuperClass.rawType == targetType) {
