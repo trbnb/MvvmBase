b93db8b89807acda1964db99821cbeaea4bc9a08
Thorben Buchta <thorbenbuchta@gmail.com>
2020-03-28 18:46:43 +0100

    test(viewmodel): redo ViewModel lifecycle tests

--- mvvmbase/src/test/java/de/trbnb/mvvmbase/LifecycleOwnerTests.kt
+++ mvvmbase/src/test/java/de/trbnb/mvvmbase/LifecycleOwnerTests.kt
@@ -1,75 +1,45 @@
 package de.trbnb.mvvmbase
 
-import androidx.lifecycle.GenericLifecycleObserver
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
 import org.junit.Test
 
 class ViewModelLifecycleTests {
-    /**
-     * Test [BaseViewModel] class that can be destroyed at will.
-     */
-    class TestViewModel: BaseViewModel() {
-        fun destroy() {
-            onCleared()
-        }
+    class ViewModel : BaseViewModel()
+
+    @Test
+    fun `initial state`() {
+        val viewModel = ViewModel()
+        assert(viewModel.lifecycle.currentState == Lifecycle.State.STARTED)
     }
 
-    /**
-     * Test if [GenericLifecycleObserver]s will be called with the correct event.
-     */
     @Test
-    fun genericLifecycleObserver() {
-        var lastEvent: Lifecycle.Event? = null
-        val viewModel = TestViewModel().apply {
-            lifecycle.addObserver(LifecycleEventObserver { _, event ->
-                lastEvent = event
-            })
-        }
-        viewModel.destroy()
-
-        assert(lastEvent === Lifecycle.Event.ON_DESTROY)
+    fun `after onBind()`() {
+        val viewModel = ViewModel()
+        viewModel.onBind()
+        assert(viewModel.lifecycle.currentState == Lifecycle.State.RESUMED)
     }
 
-    /**
-     * Test if other [LifecycleObserver] implementation are called properly.
-     */
     @Test
-    fun otherLifecycleObserver() {
-        var isDestroyed = false
-        var onAnyCalled = false
-        var onCreateCalled = false
-        var otherMethodCalled = false
+    fun `after onUnbind()`() {
+        val viewModel = ViewModel()
+        viewModel.onBind()
+        viewModel.onUnbind()
+        assert(viewModel.lifecycle.currentState == Lifecycle.State.STARTED)
+    }
 
-        val viewModel = TestViewModel().apply {
-            lifecycle.addObserver(object : LifecycleObserver {
-                @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-                fun onDestroyed() {
-                    isDestroyed = true
-                }
+    @Test
+    fun `after onDestroy() without binding`() {
+        val viewModel = ViewModel()
+        viewModel.onDestroy()
+        assert(viewModel.lifecycle.currentState == Lifecycle.State.DESTROYED)
+    }
 
-                @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
-                fun onAny() {
-                    onAnyCalled = true
-                }
-
-                @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-                fun onCreate() {
-                    onCreateCalled = true
-                }
-
-                fun otherMethod() {
-                    otherMethodCalled = true
-                }
-            })
-        }
-        viewModel.destroy()
-
-        assert(isDestroyed)
-        assert(onAnyCalled)
-        assert(!onCreateCalled)
-        assert(!otherMethodCalled)
+    @Test
+    fun `after onDestroy() with binding`() {
+        val viewModel = ViewModel()
+        viewModel.onBind()
+        viewModel.onUnbind()
+        viewModel.onDestroy()
+        assert(viewModel.lifecycle.currentState == Lifecycle.State.DESTROYED)
     }
 }
