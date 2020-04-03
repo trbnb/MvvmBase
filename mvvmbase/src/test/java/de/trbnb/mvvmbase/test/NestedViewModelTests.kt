e7024eecb2086458f364e3f88fe712e266480ac6
Thorben Buchta <thorbenbuchta@gmail.com>
2020-04-03 22:16:25 +0200

    test(viewmodel): add unit tests for autoDestroy and bindEvents

---
+++ mvvmbase/src/test/java/de/trbnb/mvvmbase/test/NestedViewModelTests.kt
@@ -0,0 +1,41 @@
+package de.trbnb.mvvmbase.test
+
+import androidx.lifecycle.Lifecycle
+import de.trbnb.mvvmbase.BaseViewModel
+import de.trbnb.mvvmbase.events.Event
+import org.junit.Test
+
+class NestedViewModelTests {
+    @Test
+    fun `autoDestroy() destroys ViewModels in a "parent" ViewModel`() {
+        val childViewModel = SimpleViewModel()
+        val parentViewModel = object : BaseViewModel() {
+            val items = listOf(childViewModel).apply { autoDestroy() }
+        }
+
+        parentViewModel.onDestroy()
+        assert(childViewModel.lifecycle.currentState == Lifecycle.State.DESTROYED)
+    }
+
+    @Test
+    fun `bindEvents() redirects events from "child" ViewModels to eventChannel of "parent" ViewModel`() {
+        val event = object : Event {}
+        val childViewModel = SimpleViewModel()
+        val parentViewModel = object : BaseViewModel() {
+            val items = listOf(childViewModel).bindEvents()
+        }
+
+        var eventWasReceived = false
+
+        parentViewModel.eventChannel.addListener { newEvent ->
+            if (event == newEvent) {
+                eventWasReceived = true
+            }
+        }
+
+        childViewModel.eventChannel(event)
+        assert(eventWasReceived)
+    }
+}
+
+class SimpleViewModel : BaseViewModel()
