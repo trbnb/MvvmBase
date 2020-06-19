702ca87e4049070f3443bca1cac8e441cd486a9e
Thorben Buchta <thorbenbuchta@gmail.com>
2020-06-19 09:42:24 +0200

    style(sample): fix detekt issue in sample app

--- app/src/main/java/de/trbnb/apptemplate/second/SecondController.kt
+++ app/src/main/java/de/trbnb/apptemplate/second/SecondController.kt
@@ -1,7 +1,7 @@
 package de.trbnb.apptemplate.second
 
 import androidx.activity.ComponentActivity
-import dagger.hilt.android.internal.lifecycle.DefaultViewModelFactories.getActivityFactory
+import dagger.hilt.android.internal.lifecycle.DefaultViewModelFactories
 import de.trbnb.apptemplate.R
 import de.trbnb.mvvmbase.conductor.MvvmController
 import de.trbnb.mvvmbase.conductor.activityViewModels
@@ -9,6 +9,7 @@ import de.trbnb.mvvmbase.conductor.activityViewModels
 class SecondController : MvvmController<SecondViewModel>() {
     override val layoutId: Int = R.layout.fragment_second
     override val viewModelDelegate = activityViewModels<SecondViewModel> {
-        getActivityFactory(activity as? ComponentActivity ?: throw RuntimeException("Can't retrieve Activity")) ?: defaultViewModelProviderFactory
+        DefaultViewModelFactories.getActivityFactory(activity as? ComponentActivity ?: throw IllegalStateException("Can't retrieve Activity"))
+            ?: defaultViewModelProviderFactory
     }
 }
