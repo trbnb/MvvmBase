b1d9e3b3a973fbd62bedeed778d35060ab3e1710
Thorben Buchta <thorbenbuchta@gmail.com>
2020-08-10 20:01:12 +0200

    chore(release): release version 2.0.0-rc3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -18,8 +18,8 @@ object Android {
 }
 
 object Publishing {
-    const val versionName = "2.0.0-rc2"
-    const val versionCode = 37
+    const val versionName = "2.0.0-rc3"
+    const val versionCode = 38
 
     const val url = "https://github.com/trbnb/MvvmBase"
     const val gitUrl = "https://github.com/trbnb/MvvmBase.git"
