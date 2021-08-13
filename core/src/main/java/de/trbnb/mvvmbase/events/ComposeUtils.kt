84b3d6b62c17eb620be7d28bfcbc90fca2da92f8
Thorben Buchta <thorbenbuchta@gmail.com>
2021-08-13 20:56:04 +0200

    feat(events): EventChannelOwner.OnEvent now takes a composable function

--- core/src/main/java/de/trbnb/mvvmbase/events/ComposeUtils.kt
+++ core/src/main/java/de/trbnb/mvvmbase/events/ComposeUtils.kt
@@ -12,20 +12,23 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
  * Helper function to use handle one-off events in Compose.
  */
 @Composable
-fun EventChannelOwner.OnEvent(action: (event: Event) -> Unit) {
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(key1 = this, lifecycleOwner) {
-        eventChannel.addListener(lifecycleOwner, action)
-        onDispose { eventChannel.removeListener(action) }
-    }
+fun EventChannelOwner.OnEvent(action: @Composable (event: Event) -> Unit) {
+    val lastEvent = lastEventAsState()
+    lastEvent.value?.let { action(it) }
 }
 
 /**
  * Observes the events from [EventChannelOwner.eventChannel] as Compose state.
+ * If no event has been observed the State contains `null`.
  */
 @Composable
 fun EventChannelOwner.lastEventAsState(): State<Event?> {
     val state = remember { mutableStateOf<Event?>(null, neverEqualPolicy()) }
-    OnEvent { state.value = it }
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(key1 = this, lifecycleOwner) {
+        val action = { event: Event -> state.value = event }
+        eventChannel.addListener(lifecycleOwner, action)
+        onDispose { eventChannel.removeListener(action) }
+    }
     return state
 }
