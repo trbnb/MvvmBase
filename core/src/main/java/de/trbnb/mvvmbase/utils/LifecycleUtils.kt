87e5b9358e52c8af4ea83c5f8f75285379314701
Thorben Buchta <thorbenbuchta@gmail.com>
2021-09-18 14:36:03 +0200

    fix(style): add missing documentation and fix formatting in LifecycleUtils.kt

--- core/src/main/java/de/trbnb/mvvmbase/utils/LifecycleUtils.kt
+++ core/src/main/java/de/trbnb/mvvmbase/utils/LifecycleUtils.kt
@@ -3,5 +3,8 @@ package de.trbnb.mvvmbase.utils
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 
+/**
+ * Returns `true` if [LifecycleOwner.getLifecycle] is in a destroyed state.
+ */
 val LifecycleOwner.isDestroyed: Boolean
     get() = lifecycle.currentState == Lifecycle.State.DESTROYED
